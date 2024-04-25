1322b0b715058afec458980da232326bed98f1a4
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-04-25 18:40:22 +0530

    add private route for profile page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="bg-slate-200">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -15,10 +17,19 @@ export default function Header() {
           <Link to={"/about"}>
             <li>About</li>
           </Link>
-
-          <Link to={"/sign-in"}>
-            <li>Sign In</li>
-          </Link>
+          {currentUser ? (
+            <Link to={"/profile"}>
+              <img
+                className="h-8 w-8 rounded-full object-cover"
+                src={currentUser.photo}
+                alt="profile"
+              />
+            </Link>
+          ) : (
+            <Link to={"/sign-in"}>
+              <li>Sign In</li>
+            </Link>
+          )}
         </ul>
       </div>
     </div>
